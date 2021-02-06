8e026ade67a323523233cee14fa7f47de425688b
Suparno Deb <suparnodeb@hotmail.co.uk>
2021-02-06 17:12:04 +0000

    new commit

--- Java/sql/helpful_code.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER java_statistics IDENTIFIED BY 'java_statistics'; -- Made User for java application
-GRANT ALL ON `world`.* TO 'java_statistics';
-
-mariadb --user=java_statistics --password=java_statistics;
-mariadb -N -B world < ../sql/get_city.sql > sql_response -- 
-
