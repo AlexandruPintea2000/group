b0e7b1915116ce712b1c6bc17a93b10c98705345
Suparno Deb <suparnodeb@hotmail.co.uk>
2021-02-07 15:24:18 +0000

    jar dependencies

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class Main {
 
     public static void main(String[] args){
+        // Test SQL Connection
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "admin");
