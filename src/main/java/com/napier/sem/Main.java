31276bb1915d5eb1004140312c51266cf5dad57f
Suparno Deb <suparnodeb@hotmail.co.uk>
2021-02-07 15:23:10 +0000

    updated readme

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,8 +1,23 @@
 package com.napier.sem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "admin");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select Name from world.country");
+            while(rs.next())
+                System.out.println(rs.getString(1));
+            con.close();
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
