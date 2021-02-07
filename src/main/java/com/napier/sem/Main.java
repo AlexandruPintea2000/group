340ae96219f2ccd3ecdb7bfa6656fab80b94b56e
Suparno Deb <suparnodeb@hotmail.co.uk>
2021-02-07 17:34:19 +0000

    jar dependencies

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,9 +1,9 @@
 package com.napier.sem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import com.mysql.*;
+import com.mysql.cj.MysqlConnection;
+import com.mysql.cj.jdbc.MysqlXAConnection;
 
 public class Main {
 
