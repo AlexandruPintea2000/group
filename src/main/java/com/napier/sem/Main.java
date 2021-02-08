0b45954d6a01cfb6fd6a99d88928c7acb69f2c4c
Stephen Messer <40314216@live.napier.ac.uk>
2021-02-08 21:39:51 +0000

    added comments

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+
+        // present users choices
         int choice;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please select one of the following options: ");
         System.out.println("1. Generate all the countries in the world organised by largest population to smallest");
         System.out.println("2. Generate all the countries in a continent organised by largest population to smallest");
         System.out.println("3. Generate all the countries in a region organised by largest population to smallest.");
+        // get input from user
         choice = sc.nextInt();
 
         System.out.println("You selected " + choice);
