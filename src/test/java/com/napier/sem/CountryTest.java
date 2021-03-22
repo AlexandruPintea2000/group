d1d2472bcd0fbca745c6cf0d259cce69026ff4d5
Your Name <suparnodeb@hotmail.co.uk>
2021-03-22 18:30:50 +0000

    unit testing

--- src/test/java/com/napier/sem/CountryTest.java
+++ src/test/java/com/napier/sem/CountryTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.sql.Connection;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CountryTest {
     static Country country;
     static App app;
+    private static Connection con = null;
 
     @BeforeAll
     static void init(){
@@ -16,6 +19,7 @@ public class CountryTest {
         app = new App();
         app.connect();
         country.setCountryCode("BRA");
+        country.setCon(con);
     }
 
     @Test
