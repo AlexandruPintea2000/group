36fcb281c80eae1636342402f947a4fe4b502d6f
Your Name <suparnodeb@hotmail.co.uk>
2021-03-22 18:53:05 +0000

    unit testing

--- src/test/java/com/napier/sem/CountryTest.java
+++ src/test/java/com/napier/sem/CountryTest.java
@@ -23,38 +23,10 @@ public class CountryTest {
 
     @Test
     void getCountryNameTest(){
-        CountryTest db = new CountryTest();
-        db.connect();
-        country.setCon(db.con);
+        App app = new App();
+        app.connect();
+        country.setCon(app.con);
         country.setCountryCode("BRA");
         assertEquals("Brazil", country.getCountryName());
-    }
-
-    public void connect() {
-        try {
-            // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to database...");
-            try {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
     }
 }
