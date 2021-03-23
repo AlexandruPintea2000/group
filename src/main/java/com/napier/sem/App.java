681a365ff7528e3c817801c82e07cb3db4b7de98
Your Name <suparnodeb@hotmail.co.uk>
2021-03-23 11:12:10 +0000

    unit testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -23,8 +23,12 @@ public class App {
         city.setCityID(4079);
 
         app.generateCityReport(city);
-        app.printList(app.generateLargestToSmallest());
-
+        app.printCityList(app.generateCityLargestToSmallest());
+        app.printCityList(app.generateCapitalPopulationInContinent("Asia"));
+        app.printCityList(app.generateCityPopulation("IND"));
+        app.printCityList(app.generateCityPopulationInContinent("Europe"));
+        app.printCityList(app.generateCityTopN(5));
+        app.printCityList(app.generateTopNCapitalCities(5));
         app.disconnect();
     }
 
@@ -84,8 +88,11 @@ public class App {
     }
 
     /**
-     ********************************************** METHODS FOR CITY *********************************************
+     *******************************************************************************************************************
+     ********************************************** METHODS FOR CITY ***************************************************
+     *******************************************************************************************************************
      */
+
     /**
      Generates a full report of a city where ID is specified, in the following format
      City ID: {cityID}
@@ -122,16 +129,17 @@ public class App {
     /**
      * Generates a list of all cities (ordered from population largest - smallest)
      */
-    public ArrayList<City> generateLargestToSmallest(){
+    public ArrayList<City> generateCityLargestToSmallest(){
         ArrayList<City> cityList = new ArrayList<City>();
         try{
             Statement stmt = con.createStatement();
             String strSelect =
-                    "select ID, Name, Population from world.city order by Population desc";
+                    "select ID, CountryCode, Name, Population from world.city order by Population desc";
             ResultSet rset = stmt.executeQuery(strSelect);
             while(rset.next()){
                 City city = new City();
                 city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
                 city.setCityName(rset.getString("Name"));
                 city.setCityPopulation(rset.getLong("Population"));
                 cityList.add(city);
@@ -146,16 +154,17 @@ public class App {
      * Generates a list of the top N populated cities (where N is specified by the user)
      * @param number
      */
-    public ArrayList<City> generateTopN(int number){
+    public ArrayList<City> generateCityTopN(int number){
         ArrayList<City> cityList = new ArrayList<City>();
         try{
             Statement stmt = con.createStatement();
             String strSelect =
-                    "select ID, Name, Population from world.city order by Population desc limit " + number;
+                    "select ID, CountryCode, Name, Population from world.city order by Population desc limit " + number;
             ResultSet rset = stmt.executeQuery(strSelect);
             while(rset.next()){
                 City city = new City();
                 city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
                 city.setCityName(rset.getString("Name"));
                 city.setCityPopulation(rset.getLong("Population"));
                 cityList.add(city);
@@ -166,12 +175,241 @@ public class App {
         return cityList;
     }
 
-    public void printList(ArrayList<City> cityList){
+    /**
+     * Generates a list of the cities in a country organised by largest population to smallest
+     * @param countryCode
+     */
+    public ArrayList<City> generateCityPopulation(String countryCode){
+        ArrayList<City> cityList = new ArrayList<City>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select ID, CountryCode, Name, Population from city where CountryCode = '" + countryCode + "' order by Population desc";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while (rset.next()){
+                City city = new City();
+                city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
+                city.setCityName(rset.getString("Name"));
+                city.setCityPopulation(rset.getLong("Population"));
+                cityList.add(city);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return cityList;
+    }
+
+    /**
+     * Generates a list on all the cities in a continent organised by largest population to smallest
+     * @param continent
+     */
+    public ArrayList<City> generateCityPopulationInContinent(String continent){
+        ArrayList<City> cityList = new ArrayList<City>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select city.ID AS 'ID', city.CountryCode AS 'CountryCode', city.Name AS 'City', country.Name AS 'Country', city.Population from city inner join country on city.CountryCode = country.Code where country.Continent = '"+continent+"' order by city.Population desc";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while (rset.next()){
+                City city = new City();
+                city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
+                city.setCityName(rset.getString("City"));
+                city.setCityPopulation(rset.getLong("Population"));
+                cityList.add(city);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return cityList;
+    }
+
+    /**
+     * Generates a list of all the capital cities in a continent organised by largest population to smallest
+     * @param continent
+     */
+    public ArrayList<City> generateCapitalPopulationInContinent(String continent){
+        ArrayList<City> cityList = new ArrayList<City>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select country.Name AS 'Country', city.ID AS 'ID', city.CountryCode AS 'CountryCode', city.Name AS 'CapitalCity', city.Population from city inner join country on city.Id = country.capital where Continent = '"+continent+"' order by city.Population desc";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while (rset.next()){
+                City city = new City();
+                city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
+                city.setCityName(rset.getString("CapitalCity"));
+                city.setCityPopulation(rset.getLong("Population"));
+                cityList.add(city);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return cityList;
+    }
+
+    /**
+     * Generate a list on the top N populated capital cities in a continent where N is provided by the user
+     * @param continent
+     * @param number
+     */
+    public ArrayList<City> generateTopNCapitalPopulationInContinent(String continent, int number){
+        ArrayList<City> cityList = new ArrayList<City>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select city.ID AS 'ID', city.CountryCode AS 'CountryCode', city.Name AS 'CapitalCity', city.Population from city inner join country on city.Id = country.capital where Continent = '"+continent+"' order by city.Population desc limit " + number;
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while (rset.next()){
+                City city = new City();
+                city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
+                city.setCityName(rset.getString("CapitalCity"));
+                city.setCityPopulation(rset.getLong("Population"));
+                cityList.add(city);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return cityList;
+    }
+
+    /**
+     * Generate a list of the top N populated capital cities in the world where N is provided by the user
+     * @param number
+     */
+    public ArrayList<City> generateTopNCapitalCities(int number){
+        ArrayList<City> cityList = new ArrayList<City>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select city.ID AS 'ID', city.CountryCode AS 'CountryCode', city.Name AS 'CapitalCity', city.Population from city inner join country on city.Id = country.capital order by city.Population desc limit " + number;
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while (rset.next()) {
+                City city = new City();
+                city.setCityID(rset.getInt("ID"));
+                city.setCountryCode(rset.getString("CountryCode"));
+                city.setCityName(rset.getString("CapitalCity"));
+                city.setCityPopulation(rset.getLong("Population"));
+                cityList.add(city);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return cityList;
+    }
+
+    /**
+     * Prints the ArrayList containing information regarding City to console
+     * @param cityList
+     */
+    public void printCityList(ArrayList<City> cityList){
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "ID", "CountryCode", "City", "Population"));
         for(City city : cityList){
-            System.out.println(city.getCityName() + " \t\t\t\t " + city.getCityPopulation());
+            //System.out.println(city.getCityName() + " \t\t\t\t " + city.getCityPopulation());
         }
     }
+
     /**
-     ********************************************** END OF METHODS FOR CITY *********************************************
+     *******************************************************************************************************************
+     ********************************************** END OF METHODS FOR CITY ********************************************
+     *******************************************************************************************************************
+     */
+
+
+    /**
+     *******************************************************************************************************************
+     ********************************************** METHODS FOR COUNTRY ************************************************
+     *******************************************************************************************************************
+     */
+
+    /**
+     Generates a full report of a country where code is specified, in the following format
+     Country Code: {countryCode}
+     Country Name: {countryName}
+     Country Continent: {countryContinent}
+     Country Region: {countryRegion}
+     Country Population: {countryPopulation}
+     Country Capital: {countryCapital}
+     */
+    public void generateCountryReport(Country country){
+        try {
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select * from world.country where Code = '" + country.getCountryCode() + "'";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            if(rset.next()){
+                country.setCountryName(rset.getString("Name"));
+                country.setCountryContinent(rset.getString("Continent"));
+                country.setCountryRegion(rset.getString("Region"));
+                country.setCountryPopulation(rset.getLong("Population"));
+                country.setCountryCapital(rset.getInt("Capital"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Country Code: " + country.getCountryCode());
+        System.out.println("Country Name: " + country.getCountryName());
+        System.out.println("Country Continent: " + country.getCountryContinent());
+        System.out.println("Country Region: " + country.getCountryRegion());
+        System.out.println("Country Population: " + country.getCountryPopulation());
+        System.out.println("Country Capital: " + country.getCountryCapital());
+    }
+
+    /**
+     * Generates a list of all countries (ordered from population largest - smallest)
+     */
+    public ArrayList<Country> generateCountryLargestToSmallest(){
+        ArrayList<Country> countryList = new ArrayList<Country>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select Code, Name, Population from world.country order by Population desc";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while(rset.next()){
+                Country ctry = new Country();
+                ctry.setCountryCode(rset.getString("Code"));
+                ctry.setCountryName(rset.getString("Name"));
+                ctry.setCountryPopulation(rset.getLong("Population"));
+                countryList.add(ctry);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return countryList;
+    }
+
+    /**
+     * Generates a list of the top N populated countries (where N is specified by the user)
+     * @param number
+     */
+    public ArrayList<Country> generateCountryTopN(int number){
+        ArrayList<Country> countryList = new ArrayList<Country>();
+        try{
+            Statement stmt = con.createStatement();
+            String strSelect =
+                    "select Code, Name, Population from world.country order by Population desc limit " + number;
+            ResultSet rset = stmt.executeQuery(strSelect);
+            while(rset.next()){
+                Country ctry = new Country();
+                ctry.setCountryCode(rset.getString("Code"));
+                ctry.setCountryName(rset.getString("Name"));
+                ctry.setCountryPopulation(rset.getLong("Population"));
+                countryList.add(ctry);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return countryList;
+    }
+
+
+    /**
+     *******************************************************************************************************************
+     ********************************************** END OF METHODS FOR COUNTRY *****************************************
+     *******************************************************************************************************************
      */
 }
--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
  * @author Suparno Deb, Alexandru Pintea, Stephen Messer, Vassilis Papadodimas
  */
 public class City {
-    // Placeholder for SQL Connection
-    private Connection con;
     // City ID
     private int cityID;
     // City name
@@ -48,196 +46,99 @@ public class City {
 
     }
 
-    public Connection getCon() {
-        return con;
-    }
-
-    public void setCon(Connection con) {
-        this.con = con;
-    }
-
+    /**
+     * Returns the City ID
+     * @return cityID
+     */
     public int getCityID() {
         return cityID;
     }
 
+    /**
+     * Sets a value for City ID
+     * @param cityID
+     */
     public void setCityID(int cityID) {
         this.cityID = cityID;
     }
 
+    /**
+     * Returns the City Name
+     * @return cityName
+     */
     public String getCityName() {
         return cityName;
     }
 
+    /**
+     * Sets a value for City Name
+     * @param cityName
+     */
     public void setCityName(String cityName) {
         this.cityName = cityName;
     }
 
+    /**
+     * Returns the Country Code
+     * @return countryCode
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * Sets a value for Country Code
+     * @param countryCode
+     */
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
     }
 
+    /**
+     * Returns the Country Name
+     * @return countryName
+     */
     public String getCountryName() {
         return countryName;
     }
 
+    /**
+     * Sets a value for Country Name
+     * @param countryName
+     */
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
 
+    /**
+     * Returns the City District
+     * @return cityDistrict
+     */
     public String getCityDistrict() {
         return cityDistrict;
     }
 
+    /**
+     * Sets a value for City District
+     * @param cityDistrict
+     */
     public void setCityDistrict(String cityDistrict) {
         this.cityDistrict = cityDistrict;
     }
 
+    /**
+     * Returns the City Population
+     * @return cityPopulation
+     */
     public long getCityPopulation() {
         return cityPopulation;
     }
 
+    /**
+     * Sets a value for City Population
+     * @param cityPopulation
+     */
     public void setCityPopulation(long cityPopulation) {
         this.cityPopulation = cityPopulation;
-    }
-
-    /**
-     * Generates a list of the cities in a country organised by largest population to smallest
-     * @param countryCode
-     */
-    public void generateCityPopulation(String countryCode){
-        ArrayList<City> cityList = new ArrayList<City>();
-        try{
-            Statement stmt = con.createStatement();
-            String strSelect =
-                    "select Name, Population from city where CountryCode = '" + countryCode + "' order by Population desc";
-            ResultSet rset = stmt.executeQuery(strSelect);
-            while (rset.next()){
-                City city = new City();
-                city.cityName = rset.getString("Name");
-                city.cityPopulation = rset.getLong("Population");
-                cityList.add(city);
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        System.out.println("CITIES");
-        System.out.println("Name \t\t\t\t Population");
-        for(City city : cityList){
-            System.out.println(city.cityName + " \t\t\t\t " + city.cityPopulation);
-        }
-    }
-
-    /**
-     * Generates a list on all the cities in a continent organised by largest population to smallest
-     * @param continent
-     */
-    public void generateCityPopulationInContinent(String continent){
-        ArrayList<City> cityList = new ArrayList<City>();
-        try{
-            Statement stmt = con.createStatement();
-            String strSelect =
-                    "select city.Name AS 'City', country.Name AS 'Country', city.Population from city inner join country on city.CountryCode = country.Code where country.Continent = '"+continent+"' order by city.Population desc";
-            ResultSet rset = stmt.executeQuery(strSelect);
-            while (rset.next()){
-                City city = new City();
-                city.cityName = rset.getString("City");
-                city.countryName = rset.getString("Country");
-                city.cityPopulation = rset.getLong("Population");
-                cityList.add(city);
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        System.out.println("CITY REPORTS IN " + continent);
-        System.out.println("City \t\t\t\t Country \t\t\t\t Population");
-        for(City city : cityList){
-            System.out.println(city.cityName + " \t\t\t\t " + city.countryName + " \t\t\t\t " + city.cityPopulation);
-        }
-    }
-
-    /**
-     * Generates a list of all the capital cities in a continent organised by largest population to smallest
-     * @param continent
-     */
-    public void generateCapitalPopulationInContinent(String continent){
-        ArrayList<City> cityList = new ArrayList<City>();
-        try{
-            Statement stmt = con.createStatement();
-            String strSelect =
-                    "select country.Name AS 'Country', city.Name AS 'CapitalCity', city.Population from city inner join country on city.Id = country.capital where Continent = '"+continent+"' order by city.Population desc";
-            ResultSet rset = stmt.executeQuery(strSelect);
-            while (rset.next()){
-                City city = new City();
-                city.cityName = rset.getString("CapitalCity");
-                city.cityPopulation = rset.getLong("Population");
-                cityList.add(city);
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        System.out.println("CAPITAL CITIES IN " + continent);
-        System.out.println("City \t\t\t\t Population");
-        for(City city : cityList){
-            System.out.println(city.cityName + " \t\t\t\t " + city.cityPopulation);
-        }
-    }
-
-    /**
-     * Generate a list on the top N populated capital cities in a continent where N is provided by the user
-     * @param continent
-     * @param number
-     */
-    public void generateTopNCapitalPopulationInContinent(String continent, int number){
-        ArrayList<City> cityList = new ArrayList<City>();
-        try{
-            Statement stmt = con.createStatement();
-            String strSelect =
-                    "select city.Name AS 'CapitalCity', city.Population from city inner join country on city.Id = country.capital where Continent = '"+continent+"' order by city.Population desc limit " + number;
-            ResultSet rset = stmt.executeQuery(strSelect);
-            while (rset.next()){
-                City city = new City();
-                city.cityName = rset.getString("CapitalCity");
-                city.cityPopulation = rset.getLong("Population");
-                cityList.add(city);
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        System.out.println("TOP " + number + " POPULATED CAPITAL CITIES IN " + continent);
-        System.out.println("City \t\t\t\t Population");
-        for(City city : cityList){
-            System.out.println(city.cityName + " \t\t\t\t " + city.cityPopulation);
-        }
-    }
-
-    /**
-     * Generate a list of the top N populated capital cities in the world where N is provided by the user
-     * @param number
-     */
-    public void generateTopNCapitalCities(int number){
-        ArrayList<City> cityList = new ArrayList<City>();
-        try{
-            Statement stmt = con.createStatement();
-            String strSelect =
-                    "select city.Name AS 'CapitalCity', city.Population from city inner join country on city.Id = country.capital order by city.Population desc limit " + number;
-            ResultSet rset = stmt.executeQuery(strSelect);
-            while (rset.next()) {
-                City city = new City();
-                city.cityName = rset.getString("CapitalCity");
-                city.cityPopulation = rset.getLong("Population");
-                cityList.add(city);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        System.out.println("TOP " + number + " POPULATED CAPITAL CITIES IN THE WORLD");
-        System.out.println("City \t\t\t\t Population");
-        for(City city : cityList){
-            System.out.println(city.cityName + " \t\t\t\t " + city.cityPopulation);
-        }
     }
 }
