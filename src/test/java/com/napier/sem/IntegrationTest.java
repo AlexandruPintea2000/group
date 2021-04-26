475e93105b9da3013476dbdba26d7771d477c490
Your Name <suparnodeb@hotmail.co.uk>
2021-04-26 13:53:04 +0100

    generateLanguageReport()

--- src/test/java/com/napier/sem/IntegrationTest.java
+++ src/test/java/com/napier/sem/IntegrationTest.java
@@ -441,8 +441,8 @@ public class IntegrationTest {
 
         assertEquals("Chinese", first.getLanguage());
         assertEquals("Hindi", second.getLanguage());
-        assertEquals(5.8405, third.getPercentage());
-        assertEquals(347077867, fourth.getPopulation());
-        assertEquals(3.8468, fifth.getPercentage());
+        assertEquals(5.8404998779296875, third.getPercentage());
+        assertEquals(347077867.30000, fourth.getPopulation());
+        assertEquals(3.846831300, fifth.getPercentage());
     }
 }
